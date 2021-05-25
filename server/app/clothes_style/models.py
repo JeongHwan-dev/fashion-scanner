0e6eeb8382385eabf86ca0ac6d6ac1dd034d2376
bky373 <bo373@naver.com>
2021-05-25 23:22:12 +0900

    Feat : 의류 카테고리, 속성, 색상 모델 생성
    - clothes_style 앱 안에 ClothesCategory와 ClothesAttribute,
    - Color 모델을 생성함

---
+++ server/app/clothes_style/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class ClothesCategory(models.Model):
+    category_name_en = models.CharField(max_length=32, null=False)
+    category_name_kr = models.CharField(max_length=32, null=False)
+
+    class Meta:
+        db_table = 'clothes_category'
+        verbose_name = '의류 카테고리'
+        verbose_name_plural = '의류 카테고리'
+
+
+class ClothesAttributes(models.Model):
+    attribute_name_en = models.CharField(max_length=32, null=False)
+    attribute_name_kr = models.CharField(max_length=32, null=False)
+
+    class Meta:
+        db_table = 'clothes_attribute'
+        verbose_name = '의류 속성'
+        verbose_name_plural = '의류 속성'
+
+
+class Color(models.Model):
+    hex_code = models.CharField(max_length=32, null=False)
+
+    class Meta:
+        db_table = 'color'
+        verbose_name = '색상'
+        verbose_name_plural = '색상'
--- server/app/entrypoint.sh
+++ server/app/entrypoint.sh
@@ -11,7 +11,7 @@ then
     echo "PostgreSQL started"
 fi
 
-python manage.py flush --no-input
+# python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
 
