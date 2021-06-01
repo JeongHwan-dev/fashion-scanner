8d79c4043bdda998e7180cb749239ee4f5a7de52
bky373 <bo373@naver.com>
2021-06-01 21:49:02 +0900

    Feat : static, media 파일에 접근 가능하도록 변경
    - ShopClothes 모델에서 UPLOAD_PATH 변경
    - static 파일의 위치를 찾기 위해 STATICFILES_DIRS 경로 추가
    - 위치를 중복해서 찾는 걸 방지하기 위해  STATICFILES_FINDERS 경로 추가
    - static, media 파일에 접근할 수 있도록 STATIC_URL, MEDIA_URL 경로 추가

--- server/app/fashion_scanner/urls.py
+++ server/app/fashion_scanner/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from django.conf import settings
 from member import views
 
@@ -27,3 +28,6 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
