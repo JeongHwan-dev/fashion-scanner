17f7453fa9de7a1a51f0391df53eed2c821a3921
Park Jeong Hwan <jeonghwan.dev@gmail.com>
2021-06-01 14:04:25 +0900

    Style : 룩북 멤버 버튼 이미지 수정, 섹션 패딩 조정

--- client/src/components/matchingComponents/FailCard.js
+++ client/src/components/matchingComponents/FailCard.js
@@ -17,7 +17,7 @@ const FailCard = () => {
         <div className="failCard__bg">
           <img src="/images/failure_group_photo.jpg" alt="Failure" />
           <div className="failCard__dsc">
-            <p>매칭이 실패하였습니다.</p>
+            <p>매칭에 실패하였습니다.</p>
             <p>다시 한번 시도해 주세요</p>
             <p>매칭이 계속 실패하신다면 다른 사진을 활용해 주세요.</p>
           </div>
