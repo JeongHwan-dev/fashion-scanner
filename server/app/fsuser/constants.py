6b60f10e732a75d58c004675d499dc62fca11da3
bky373 <bo373@naver.com>
2021-06-09 22:45:05 +0900

    Style : 멤버 매칭 결과 글 스타일 변경

--- server/app/fsuser/constants.py
+++ server/app/fsuser/constants.py
@@ -3,7 +3,7 @@ MEMBER_DESCRIPTIONS_EN = {
         "name": "jisoo",
         "title": "You got Jisoo!",
         "comment1": "You have such great fashion sense.",
-        "comment2": "See the LOOKBOOK of JIsoo.",
+        "comment2": "See the LOOKBOOK of Jisoo.",
         "btnLabel": "LOOKBOOK",
     },
     "jennie": {
@@ -33,30 +33,30 @@ MEMBER_DESCRIPTIONS_EN = {
 MEMBER_DESCRIPTIONS_KO = {
     "jisoo": {
         "name": "jisoo",
-        "title": "당신은 패완얼의 정석 지수.",
+        "title": "당신은 패완얼의 정석 지수!",
         "comment1": "남다른 패션센스를 자랑하지만 가끔 얼굴때문에 그 센스가 묻히는군요.",
-        "comment2": "지수의 룩북을 참고해보세요 !",
+        "comment2": "지수의 룩북을 참고해보세요.",
         "btnLabel": "지수 룩북 바로가기",
     },
     "jennie": {
         "name": "jennie",
-        "title": "당신은 인간명품 제니.",
+        "title": "당신은 인간명품 제니!",
         "comment1": "캐쥬얼과 화려함 사이를 오고가는 변신의 귀재이시군요.",
-        "comment2": "제니의 룩북을 참고해보세요 !",
+        "comment2": "제니의 룩북을 참고해보세요.",
         "btnLabel": "제니 룩북 바로가기",
     },
     "lisa": {
         "name": "lisa",
-        "title": "당신은 비율 천재 리사.",
+        "title": "당신은 비율 천재 리사!",
         "comment1": "남다른 각선미로 어떤 옷이던 자기 옷처럼 만드는 남다른 소화력을 지니셨군요.",
-        "comment2": "리사의 룩북을 참고해보세요 !",
+        "comment2": "리사의 룩북을 참고해보세요.",
         "btnLabel": "리사 룩북 바로가기",
     },
     "rose": {
         "name": "rose",
-        "title": "당신은 분위기 여신 로제.",
+        "title": "당신은 분위기 여신 로제!",
         "comment1": "편안한 룩을 즐기지만 항상 컬러로 포인트를 주는 꾸안꾸의 정석이시군요.",
-        "comment2": "로제의 룩북을 참고해보세요 !",
+        "comment2": "로제의 룩북을 참고해보세요.",
         "btnLabel": "로제 룩북 바로가기",
     },
 }
