b527fdaaed4dc78d302a4b9a6dd9aa9c9d912ebd
Park Jeong Hwan <jeonghwan.dev@gmail.com>
2021-06-10 20:59:06 +0900

    Fix : setupProxy 수정

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,11 @@
 const createProxyMiddleware = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-            target: 'http://elice-kdt-ai-track-vm-ai-13.koreacentral.cloudapp.azure.com:8000',
-            changeOrigin: true
-        })
-    );
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://elice-kdt-ai-track-vm-distribute-13.koreacentral.cloudapp.azure.com:8001',
+      changeOrigin: true,
+    }),
+  );
 };
