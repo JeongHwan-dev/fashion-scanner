4016e017fec03190598a4f5565a35d98b1657254
Park Jeong Hwan <jeonghwan.dev@gmail.com>
2021-06-11 20:07:47 +0900

    Fix : 로딩 및 결과 페이지 네비게이션 바 수정

---
+++ client/src/components/commonComponents/NavigationMatching.js
@@ -0,0 +1,79 @@
+import React, { useContext, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { useTranslation } from 'react-i18next';
+import { LangContext } from 'context/LanguageContext';
+import 'components/commonComponents/css/NavigationMatching.css';
+
+const NavigationMatching = () => {
+  const isMobile = useMediaQuery({ maxWidth: 500 });
+  const [menu, setMenu] = useState(false); // 햄버거 버튼
+  const { setEng, setKor } = useContext(LangContext); // 전역으로 상태 관리: 현재 언어, 영어로 지정, 한국어로 지정
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <header>
+        <nav className="inner">
+          <a href="/" className="logo">
+            <img src="/images/common/fs_s3_logo.png" alt="FASHION SCANNER" />
+          </a>
+          <span
+            className="material-icons"
+            onClick={() => {
+              setMenu(!menu);
+            }}
+          >
+            menu
+          </span>
+          <div className={`link-group ${menu === true ? 'active' : ''}`}>
+            <ul className="link">
+              {isMobile && (
+                <>
+                  <li className="lang__a">
+                    <a
+                      className={`lang ${localStorage.i18nextLng === 'ko' ? 'active' : ''}`}
+                      onClick={setKor}
+                    >
+                      KOR
+                    </a>
+                    <span>|</span>
+                    <a
+                      className={`lang ${localStorage.i18nextLng === 'en' ? 'active' : ''}`}
+                      onClick={setEng}
+                    >
+                      ENG
+                    </a>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+          <div className="language-group">
+            <ul className="language">
+              {!isMobile && (
+                // pc 화면일 때 나오는 다국어
+                <>
+                  <li
+                    className={`lang ${localStorage.i18nextLng === 'ko' ? 'active' : ''}`}
+                    onClick={setKor}
+                  >
+                    KOR
+                  </li>
+                  <span>|</span>
+                  <li
+                    className={`lang ${localStorage.i18nextLng === 'en' ? 'active' : ''}`}
+                    onClick={setEng}
+                  >
+                    ENG
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default NavigationMatching;
